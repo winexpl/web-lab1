26c90dd877a62bc5a74eb34628ff3dd223ed37ad
Marusya <drevina2003@mail.ru>
2025-02-15 21:37:01 +0400

    add FIND  with namedParameterJbdcTemplate

--- src/main/java/com/lab1/repository/JdbcProjectRepository.java
+++ src/main/java/com/lab1/repository/JdbcProjectRepository.java
@@ -3,13 +3,16 @@ package com.lab1.repository;
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import com.lab1.entity.Project;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import lombok.RequiredArgsConstructor;
@@ -34,7 +37,6 @@ public class JdbcProjectRepository implements ProjectRepository {
 
     @Override
     public Project save(Project project) {
-        System.out.println(project.getId());
         String sqlRequest = "INSERT INTO project (p_name, p_descr, p_begin_date, p_end_date) VALUES (?, ?, ?, ?) RETURNING p_id";
         var id = jdbcTemplate.queryForObject(sqlRequest, Integer.class,
             project.getName(),
@@ -71,7 +73,7 @@ public class JdbcProjectRepository implements ProjectRepository {
     public Project findById(int id) {
         String sqlRequest = "SELECT * FROM project WHERE p_id=?";
         try {
-            return this.jdbcTemplate.queryForObject(sqlRequest, projectMapper, id);
+            return jdbcTemplate.queryForObject(sqlRequest, projectMapper, id);
         } catch (Exception e) {
             return null;
         }
@@ -80,9 +82,13 @@ public class JdbcProjectRepository implements ProjectRepository {
     @Override
     public List<Project> findByRangeOfDates(LocalDate start_date, LocalDate end_date) {
         String sqlRequest = "SELECT * FROM project WHERE " +
-            "(p_begin_date BETWEEN ? AND ?) " +
-            "AND (p_end_date BETWEEN ? AND ?)";
-        List<Project> project = this.jdbcTemplate.query(sqlRequest, projectMapper, start_date, end_date, start_date, end_date);
+            "(p_begin_date BETWEEN :start_date AND :end_date) " +
+            "AND (p_end_date BETWEEN :start_date AND :end_date)";
+        SqlParameterSource map = new MapSqlParameterSource()
+            .addValue("start_date", start_date)
+            .addValue("end_date", end_date);
+
+        List<Project> project = namedParameterJdbcTemplate.query(sqlRequest, map, projectMapper);
         return project;
     }
 
